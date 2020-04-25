31a91757d53586995924712b1052a734cd451fad
ganeshShetty98 <gs00619@gmail.com>
2020-04-25 10:34:57 +0530

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,38 @@
+/*js eroor detector*/
+
+window.onerror = function (msg, url, LineNo, columnNo, error) {
+  alert(
+    "An error occured on" +
+      "Line no : " +
+      LineNo +
+      "  Column no : " +
+      columnNo +
+      "\n\nMessage : " +
+      msg +
+      "\n\nerror : " +
+      error +
+      "\n\nUrl : " +
+      url
+  );
+};
+
+/*js eroor detector*/
+
+$(document).ready(function () {
+  $nav = $(".nav");
+  $toggleCollapse = $(".toggle-collapse");
+
+  /** Click event on togglemenu*/
+  $toggleCollapse.click(function () {
+    $nav.toggleClass("collapse");
+  });
+
+  //owl-carousel
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    dots: false,
+    nav: true,
+  });
+});
