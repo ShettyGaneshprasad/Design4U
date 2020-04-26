2672f57b694b37e500baeb81919ec020dfe5e858
ganeshShetty98 <gs00619@gmail.com>
2020-04-26 09:59:18 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -30,9 +30,13 @@ $(document).ready(function () {
   //owl-carousel
   $(".owl-carousel").owlCarousel({
     loop: true,
-    autoplay: true,
+    autoplay: false,
     autoplayTimeout: 2000,
     dots: false,
     nav: true,
+    navText: [
+      $(".owl-navigation .owl-nav-prev"),
+      $(".owl-navigation .owl-nav-next"),
+    ],
   });
 });
