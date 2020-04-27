8f9ec41e8a294f1035d8180eaa6da1459719bb6a
ganeshShetty98 <gs00619@gmail.com>
2020-04-27 19:56:32 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -18,6 +18,21 @@ window.onerror = function (msg, url, LineNo, columnNo, error) {
 
 /*js eroor detector*/
 
+const responsive = {
+  0: {
+    items: 1,
+  },
+  320: {
+    items: 1,
+  },
+  560: {
+    items: 2,
+  },
+  960: {
+    items: 3,
+  },
+};
+
 $(document).ready(function () {
   $nav = $(".nav");
   $toggleCollapse = $(".toggle-collapse");
@@ -30,7 +45,7 @@ $(document).ready(function () {
   //owl-carousel
   $(".owl-carousel").owlCarousel({
     loop: true,
-    autoplay: false,
+    autoplay: true,
     autoplayTimeout: 2000,
     dots: false,
     nav: true,
@@ -38,5 +53,19 @@ $(document).ready(function () {
       $(".owl-navigation .owl-nav-prev"),
       $(".owl-navigation .owl-nav-next"),
     ],
+    responsive: responsive,
   });
+
+  //click to croolon top to website
+  $(".move-up span").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      1000
+    );
+  });
+
+  //animation on scrool
+  AOS.init();
 });
